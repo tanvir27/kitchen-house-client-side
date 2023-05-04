3aec01aa7ed07d0a289ca3560c15973ab8d82e25
Tanvir Hasan <87339504+tanvir27@users.noreply.github.com>
2023-05-04 16:17:05 +0600

    firebase config-hide and set up private route

--- src/components/RecipeDetails/RecipeDetails.jsx
+++ src/components/RecipeDetails/RecipeDetails.jsx
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Card } from "react-bootstrap";
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
-
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const RecipeDetails = ({ data }) => {
-  const { recipe_image, recipe_name, rating, ingredients, cooking_method } =
-    data;
-
+    const [favorite, setFavorite] = useState(false);
+    const { recipe_image, recipe_name, rating, ingredients, cooking_method } = data;
+    
+const handleAddToFavorite = () => {
+    setFavorite(true);
+//    toast.success("Successfully toasted!");
+    toast("Add to Favorite!");
+  };
   return (
     <div>
       <Card>
         <Card.Img variant="top" src={recipe_image} />
         <Card.Body>
-          <Card.Title className="text-center  fw-bold fs-4">{recipe_name} </Card.Title>
-                  <Card.Text>
-                      <p className="fs-5">Ingredients: </p>
+          <Card.Title className="text-center  fw-bold fs-4">
+            {recipe_name}{" "}
+          </Card.Title>
+          <Card.Text>
+            <p className="fs-5">Ingredients: </p>
             <ul className="">
               <li>{ingredients[0]}</li>
               <li>{ingredients[1]}</li>
@@ -24,10 +32,13 @@ const RecipeDetails = ({ data }) => {
             </ul>
           </Card.Text>
           <div className="py-2">
-            <h5 className="">Rating: {rating}</h5>
+            <h5 className="">
+              Rating:{" "}
+              <Rating style={{ maxWidth: 100 }} value={rating} readOnly />
+            </h5>
           </div>
-                  <Card.Text>
-                      <p className="fs-5">Cooking Method:</p>
+          <Card.Text>
+            <p className="fs-5">Cooking Method:</p>
             <ul className="">
               <li>{cooking_method[0]}</li>
               <li>{cooking_method[1]}</li>
@@ -37,7 +48,14 @@ const RecipeDetails = ({ data }) => {
             </ul>
           </Card.Text>
         </Card.Body>
-        <Button variant="primary">Favorite</Button>
+        <Button
+          onClick={handleAddToFavorite}
+          disabled={favorite}
+          variant="primary"
+        >
+          Favorite
+        </Button>
+        <ToastContainer />
       </Card>
     </div>
   );
--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC1xEy8s4wyuO3LMI8GdBQ-PMtd_uuIlVw",
-  authDomain: "the-kitchen-house.firebaseapp.com",
-  projectId: "the-kitchen-house",
-  storageBucket: "the-kitchen-house.appspot.com",
-  messagingSenderId: "702144396896",
-  appId: "1:702144396896:web:248f5ff86395136da58d05",
+  apiKey: import.meta.env.VITE_APIKEY,
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
